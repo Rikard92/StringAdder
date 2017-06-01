60c3573809a4798521cb6baf83ef97309f754db6
Rikard <Rikard.Persson92@hotmail.com>
2017-06-01 15:17:04 +0200

    Add project files.

---
+++ StringAdder/Adition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAdder
+{
+    public class Adition
+    {
+        public int Add(string numbers)
+        {
+            int ret =0;
+            if (numbers.Contains(",")|| numbers.Contains("\n"))
+            {
+                char[] thecar = new[] {',', '\n'}; 
+
+                string[] sep = numbers.Split(thecar);
+                
+
+                foreach (string thes in sep)
+                {
+                    if (Convert.ToInt32(thes) < 0)
+                    {
+                        throw new NumberNegativeException();
+                    }
+
+                    ret = ret + Convert.ToInt32(thes);
+                }
+
+            }
+            else if (numbers.Equals(""))
+            {
+                
+            }
+            else
+            {
+                ret = Convert.ToInt32(numbers);
+            }
+
+
+            return ret;
+        }
+    }
+}
---
+++ StringAdderTest/AditionTest.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StringAdder;
+
+namespace StringAdderTest
+{
+    [TestFixture]
+    class AditionTest
+    {
+        private Adition sut;
+
+        [SetUp]
+        public void AditionTestSetup()
+        {
+            sut = new Adition();
+        }
+
+        [Test]
+        public void Add1()
+        {
+            //Arrange
+            int ret;
+
+            //Act
+            ret=sut.Add("");
+
+
+            //Assert
+            Assert.AreEqual(0, ret);
+
+        }
+
+        [Test]
+        public void Add2()
+        {
+            //Arrange
+            int ret;
+
+            //Act
+            ret = sut.Add("1");
+
+
+            //Assert
+            Assert.AreEqual(1, ret);
+
+        }
+
+        [Test]
+        public void Add3()
+        {
+            //Arrange
+            int ret;
+
+            //Act
+            ret = sut.Add("1,2");
+
+
+            //Assert
+            Assert.AreEqual(3, ret);
+
+        }
+
+        [Test]
+        public void Add4()
+        {
+            //Arrange
+            int ret;
+
+            //Act
+            ret = sut.Add("1,2,3");
+
+
+            //Assert
+            Assert.AreEqual(6, ret);
+
+        }
+
+        [Test]
+        public void Add5()
+        {
+            //Arrange
+            int ret;
+
+            //Act
+            ret = sut.Add("1\n2");
+
+
+            //Assert
+            Assert.AreEqual(3, ret);
+
+        }
+
+        [Test]
+        public void Add6()
+        {
+            //Arrange
+            int ret;
+
+            //Act
+            ret = sut.Add("1\n2,3");
+
+
+            //Assert
+            Assert.AreEqual(6, ret);
+
+        }
+
+        [Test]
+        public void Add7()
+        {
+            //Arrange
+            int ret;
+
+            //Act
+            ret = sut.Add("-1,2");
+
+
+            //Assert
+            Assert.AreEqual(1, ret);
+
+        }
+
+    }
+}
